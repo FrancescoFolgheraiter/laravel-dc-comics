0be3e296423a972b452de8309b425094c237ae3e
FrancescoFolgheraiter <f.folgheraiter@hotmail.it>
2024-02-29 14:31:28 +0100

    update migration create comics

--- app/Http/Controllers/Admin/ComicController.php
+++ app/Http/Controllers/Admin/ComicController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+//Model
+use App\Models\Comic;
 class ComicController extends Controller
 {
     /**
---
+++ database/migrations/2024_02_29_130534_create_comics_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',64);
+            $table->text('description')->nullable();
+            $table->text('thumb')->nullable();
+            $table->unsignedFloat('price')->nullable();
+            $table->string('series', 300)->nullable();
+            $table->date('sale_date')->nullable();
+            $table->string('type',128);
+            $table->json('artists');
+            $table->json('writers')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comics');
+    }
+};
