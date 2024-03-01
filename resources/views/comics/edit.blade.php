c005aafd3edc50a33b42665c9855733178154e58
FrancescoFolgheraiter <f.folgheraiter@hotmail.it>
2024-03-01 15:26:57 +0100

    update edit with bonus

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -6,9 +6,7 @@
 <h1 class="text-center">
    {{ $comic->title }} Edit
 </h1>
-@php
-dd($comic);
-@endphp
+
 <div class="container ">
    <div class="row">
       <div class="col py-4">
@@ -43,19 +41,29 @@ dd($comic);
            </div>
             <div class="mb-3">
                <label for="sale_date" class="form-label">Data </label>
-               <input type="date" class="form-control" id="sale_date" name="sale_date" placeholder="Inserisci la data" >
+               <input value="{{ $comic->sale_date }}" type="date" class="form-control" id="sale_date" name="sale_date" placeholder="Inserisci la data" >
             </div>
             <div class="mb-3">
                <label for="type" class="form-label">Tipo</label>
-               <input value="{{ $comic->sale_data }}" type="text" class="form-control" id="type" name="type" placeholder="Inserisci il tipo" maxlength="64">
+               <input value="{{ $comic->type }}" type="text" class="form-control" id="type" name="type" placeholder="Inserisci il tipo" maxlength="64">
             </div>
             <div class="mb-3">
+               @php
+                  //manipolo la stringa $comic->artists per pulirla e far uscire una stringa di autori separati da virgola
+                  $comic->artists = str_replace(['[', ']'], '', $comic->artists);
+                  $comic->artists = str_replace(['"', '"'], '', $comic->artists);
+               @endphp
                <label for="artists" class="form-label">Artisti</label>
-               <textarea class="form-control" id="artists" name="artists" rows="3" placeholder="Inserisci gli artisti"></textarea>
+               <textarea class="form-control" id="artists" name="artists" rows="3" placeholder="Inserisci gli artisti">{{ $comic->artists }}</textarea>
             </div>
             <div class="mb-3">
+               @php
+                  //manipolo la stringa $comic->artists per pulirla e far uscire una stringa di autori separati da virgola
+                  $comic->writers = str_replace(['[', ']'], '', $comic->writers);
+                  $comic->writers = str_replace(['"', '"'], '', $comic->writers);
+               @endphp
                <label for="writers" class="form-label">Scrittori</label>
-               <textarea class="form-control" id="writers" name="writers" rows="3" placeholder="Inserisci gli artisti"></textarea>
+               <textarea class="form-control" id="writers" name="writers" rows="3" placeholder="Inserisci gli artisti">{{ $comic->writers }}</textarea>
             </div>
             <div>
                <button type="submit" class="btn btn-success w-100">
